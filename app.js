e9f59ff3bcf3bba55e0660ea6e81154287e03d4c
Eddy Timoteo <ematix0507@gmail.com>
2024-11-21 16:57:34 -0500

    Actualizo contacto

--- app.js
+++ app.js
@@ -47,27 +47,3 @@ function loadPage(page) {
     handleHashChange();
   });
   
-  function openModal(service) {
-    const modal = document.getElementById('serviceModal');
-    const title = document.getElementById('modalTitle');
-    const description = document.getElementById('modalDescription');
-    
-    // Set dynamic content based on the service clicked
-    if (service === 'asesoria') {
-        title.innerText = "Asesoría Técnica";
-        description.innerText = "Te brindamos asesoría para elegir los mejores productos para tus proyectos. ¡Contáctanos y empieza hoy!";
-    } else if (service === 'entrega') {
-        title.innerText = "Entrega a Domicilio";
-        description.innerText = "Recibe tus productos directamente en tu casa con nuestro servicio de entrega. ¡Haz tu pedido ahora!";
-    } else if (service === 'mantenimiento') {
-        title.innerText = "Mantenimiento de Herramientas";
-        description.innerText = "Te ayudamos a mantener tus herramientas en óptimas condiciones. ¡Solicita tu mantenimiento hoy mismo!";
-    }
-
-    modal.classList.remove('hidden');
-}
-
-function closeModal() {
-    const modal = document.getElementById('serviceModal');
-    modal.classList.add('hidden');
-}
